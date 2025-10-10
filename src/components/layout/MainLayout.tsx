57f58bec85e741bb39b7003ccd0f87f6cce3fb9d
anwar <dev.anwarul@gmail.com>
2025-10-10 23:18:40 +0600

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,10 +4,10 @@ import Footer from "./Footer";
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden bg-background text-foreground">
       <Header />
 
-      <main className="flex-1">
+      <main className="flex-1 overflow-y-auto">
         <Outlet />
       </main>
 
